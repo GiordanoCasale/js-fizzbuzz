ebd82e6962a14ca77a8409b9c3c29cc9f6113f27
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-18 14:47:16 +0100

    [UPD] created some if for the calculate

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 //andiamo a creare un for per il ciclo di numeri tra 1 e 100
 for(i=0; i<=100; i++){
 
-    
+    if( i % 3 == 0 && i % 5){
+        
+        console.log("buzzfizz")
+    }
+
+    else if(i % 3 == 0){
+        
+        console.log("Fizz")
+    }
+
+    else if(i % 5 == 0){
+        console.log("Buzz")
+    }
+
+    else{
+        console.log(i + 1)
+    }
 
 }
